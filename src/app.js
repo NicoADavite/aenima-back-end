27c35a8e45049fc023086dc7f6386c8f3369eb81
Nicolás Alceo Davite <nicolasalceodavite@gmail.com>
2022-05-20 17:39:31 -0300

    make it better

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // importing external packages
 const express = require('express');
+require('dotenv').config()
 const path = require('path');
 const cors = require('cors');
 const methodOverride = require('method-override');
